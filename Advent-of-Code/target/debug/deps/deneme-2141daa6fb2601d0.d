798f71f3f8d909dd8cfc60a9de6a824f9efd1e21
Timur Sefergil <140344749+CTimurSefergil@users.noreply.github.com>
2024-02-11 03:48:15 +0300

    Add files via upload

---
+++ Advent-of-Code/target/debug/deps/deneme-2141daa6fb2601d0.d
@@ -0,0 +1,5 @@
+/home/timur/MiniDream/deneme/target/debug/deps/libdeneme-2141daa6fb2601d0.rmeta: src/main.rs
+
+/home/timur/MiniDream/deneme/target/debug/deps/deneme-2141daa6fb2601d0.d: src/main.rs
+
+src/main.rs:
---
+++ Advent-of-Code/target/debug/deps/deneme-e88e7b1f133fbff7.d
@@ -0,0 +1,5 @@
+/home/timur/MiniDream/deneme/target/debug/deps/libdeneme-e88e7b1f133fbff7.rmeta: src/main.rs
+
+/home/timur/MiniDream/deneme/target/debug/deps/deneme-e88e7b1f133fbff7.d: src/main.rs
+
+src/main.rs:
